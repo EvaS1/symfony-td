ac1303991583923e2958f1a55eae1c9116df7eec
Eva Saintier <eva.saintier@my-digital-school.org>
2019-02-14 16:12:05 +0100

    td3 commande de console

--- src/Service/SweatMessageGenerator.php
+++ src/Service/SweatMessageGenerator.php
@@ -1,17 +1,40 @@
 <?php
 
 namespace App\Service;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 
-class SweatMessageGenerator {
+class SweatMessageGenerator extends Command {
 	
 	private $idMessage;
 
-	public function __construct($idMessage) {
-		$this->idMessage = $idMessage;
+	public function __construct() {
+		/*$this->idMessage = $idMessage;*/
+
+		parent::__construct();
+	}
+
+	protected function configure() {
+		$this
+			->setName('app:sweetmessage')
+			->setDescription('A sweet message for you !')
+			->setHelp('You need to have a sweet moment. Use this service ! :D');
+
+		$this->addArgument('idMessage', InputArgument::OPTIONAL, 'L\'id du message');
+	}
+
+
+	protected function execute(InputInterface $input, OutputInterface $output) {
+		$this->idMessage = $input->getArgument('idMessage');
+		$output->writeln($this->getSweatMessage());
+		
 	}
 
 
 	public function getSweatMessage() {
+		dump($this->idMessage);
 		$messages = [
 			'Merci à toi !',
 			'Belle journée, n\'est-ce pas ?',
@@ -23,13 +46,10 @@ class SweatMessageGenerator {
 			$index = $this->idMessage;
 		} else {
 			$index = array_rand($messages);
-		}
-		
-		
+		}		
 
 		return $messages[$index];
 	}
-
 
 }
 
